00dc8c535ac5bb5a20f4b90e11957fbb1ecece29
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-02 14:23:31 +0200

    fuck this

--- begoodPlus/begoodPlus/settings/base.py
+++ begoodPlus/begoodPlus/settings/base.py
@@ -159,7 +159,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 
     # own
     #'django.middleware.locale.LocaleMiddleware',
