53f7cf3b7eea8be9cf43ce4ec5753b1ba3e7edc8
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-02 14:13:35 +0200

    servers change

--- begoodPlus/begoodPlus/settings/production.py
+++ begoodPlus/begoodPlus/settings/production.py
@@ -4,14 +4,14 @@ COMPRESS_ENABLED = False
 DEBUG=False
 
 CORS_ALLOW_CREDENTIALS = True
-
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'https://nhrnhr0.github.io',
-    'https://main.ms-global.co.il'
+    'https://test2.ms-global.co.il'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://nhrnhr0.github.io',
-    'https://main.ms-global.co.il'
+    'https://test2.ms-global.co.il'
 ]
 
 CSRF_COOKIE_SECURE = True
