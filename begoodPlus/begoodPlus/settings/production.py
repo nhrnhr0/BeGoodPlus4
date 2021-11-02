fcee65217439d328e10ec33d611f0d04e01103f1
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-02 18:08:56 +0200

    done

--- begoodPlus/begoodPlus/settings/production.py
+++ begoodPlus/begoodPlus/settings/production.py
@@ -6,15 +6,11 @@ DEBUG=False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'https://nhrnhr0.github.io',
     'https://test2.ms-global.co.il'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://nhrnhr0.github.io',
     'https://test2.ms-global.co.il'
 ]
-
-
 CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SAMESITE = 'lax'
