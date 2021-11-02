76732a90a569ed646280ae9dd5e76d0984552c32
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-02 14:40:47 +0200

    I quit

--- begoodPlus/begoodPlus/settings/production.py
+++ begoodPlus/begoodPlus/settings/production.py
@@ -14,24 +14,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://test2.ms-global.co.il'
 ]
 
-CSRF_COOKIE_SECURE = False
+'''CSRF_COOKIE_SECURE = False
 SESSION_COOKIE_SECURE = False
 CSRF_COOKIE_SAMESITE = 'lax'
-SESSION_COOKIE_SAMESITE = 'lax'
-
-'''
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
-    'http://localhost:3000',
-]
-
-CSRF_TRUSTED_ORIGINS = [
-    'localhost:3000',
-]
-
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SAMESITE = 'lax'
-SESSION_COOKIE_SAMESITE = 'None'
-'''
+SESSION_COOKIE_SAMESITE = 'lax''''
