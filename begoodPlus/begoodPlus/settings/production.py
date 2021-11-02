7b443fff555fc6d7b1ffdb4236413b80ca121b66
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-02 14:18:38 +0200

    fuck this

--- begoodPlus/begoodPlus/settings/production.py
+++ begoodPlus/begoodPlus/settings/production.py
@@ -6,12 +6,12 @@ DEBUG=False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'https://nhrnhr0.github.io',
-    'https://test2.ms-global.co.il'
+    'nhrnhr0.github.io',
+    'test2.ms-global.co.il'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://nhrnhr0.github.io',
-    'https://test2.ms-global.co.il'
+    'nhrnhr0.github.io',
+    'test2.ms-global.co.il'
 ]
 
 CSRF_COOKIE_SECURE = False
