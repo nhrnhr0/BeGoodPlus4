1fd08365b432cd5c12a32347589ddd0ac3f2a046
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-14 14:17:24 +0200

    deleteing unneeed assets

--- frontend/my-svelte-project/src/utils/globalStore.js
+++
@@ -1,6 +0,0 @@
-import { writable } from 'svelte/store'
-
-export const endpoint = window.location.href.split('/')[0] + '//' + window.location.href.split('/')[2];
-export const api_endpoint = endpoint + '/api';
-export const modal = writable(null);
-export const stockEditModal= writable(null);
