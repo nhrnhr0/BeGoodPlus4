a0ef81b501df4d6b2e8b14173cef09de1bab975d
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-16 02:29:05 +0200

    update admin actions

--- begoodPlus/catalogAlbum/admin.py
+++ begoodPlus/catalogAlbum/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from catalogAlbum.models import CatalogAlbum, ThroughImage
+from django.utils.translation import gettext_lazy  as _
 
 from catalogImages.models import CatalogImage
 from adminsortable.admin import (SortableAdmin, SortableTabularInline)
@@ -18,13 +19,23 @@ from mptt.admin import MPTTModelAdmin
 from mptt.admin import DraggableMPTTAdmin
 from django.db.models import Count
 
+
+
 class CatalogAlbumAdmin(SortableAdmin, DraggableMPTTAdmin):
     inlines = (CatalogImageInline,)
     list_display = ('tree_actions','indented_title','link_copy', 'slug', 'keywords' ,'related_images_count','is_public',)#'get_absolute_url')
     readonly_fields = ('related_images_count',)
+    
     #readonly_fields = ('get_absolute_url',)
     prepopulated_fields = {'slug': ('title',),}
     
+    def make_public(modeladmin, request, queryset):
+        queryset.update(is_public=True)
+    make_public.short_description = _('Mark selected albums as public')    
+    def make_private(modeladmin, request, queryset):
+        queryset.update(is_public=False)
+    make_private.short_description = _('Mark selected albums as private')    
+    actions  = ['make_public','make_private']
     def get_queryset(self, request):
         qs = super().get_queryset(request)
 
