38fadb6161116dbe1d766f7f5088ca94bd9a5e85
nhrnhr0 <nhrnhr0@gmail.com>
2021-11-30 17:41:37 +0200

    working on chart pie for telegram session end messages

--- begoodPlus/client/admin.py
+++ begoodPlus/client/admin.py
@@ -19,10 +19,20 @@ admin.site.register(PaymentTime, OnlyNameAdmin)
 admin.site.register(ClientOrganizations, OnlyNameAdmin)
 
 from .models import UserSessionLogger
+'''class UserLogEntryInline(admin.TabularInline):
+    model = UserSessionLogger.logs.through
+    extra = 0'''
 
 class UserSessionLoggerAdmin(admin.ModelAdmin):
     list_display = ('user', 'device', 'uniqe_color','is_active','session_start_timestemp','session_end_timestemp','session_duration',)
-    filter_horizontal = ('logs',)
+    readonly_fields = ('uid', 'user', 'device', 'uniqe_color','is_active','session_start_timestemp','session_end_timestemp','session_duration','admin_display_logs')
+    #filter_horizontal = ('logs',)
+    exclude = ('logs',)
+    #readonly_fields = ('uniqe_color','admin_display_logs',)
+    #inlines = [UserLogEntryInline]
+    def get_queryset(self, request):
+        return super(UserSessionLoggerAdmin, self).get_queryset(request).select_related(
+            'user').prefetch_related('logs')
 admin.site.register(UserSessionLogger, UserSessionLoggerAdmin)
 
 class UserLogEntryAdmin(admin.ModelAdmin):
--- begoodPlus/client/models.py
+++ begoodPlus/client/models.py
@@ -1,15 +1,22 @@
 import uuid
+from django.db.models.fields import json
 from django.utils.safestring import mark_safe
 from colorhash import ColorHash
 from django.db import models
+from catalogImages.models import CatalogImage
 from core.models import uuid2slug
 from django.db.models.fields.related import OneToOneField
 from django.utils.translation import gettext_lazy  as _
 from django.conf import settings
-
+import prettytable as pt
+import telegram
+from begoodPlus.secrects import TELEGRAM_BOT_TOKEN
 from catalogAlbum.models import CatalogAlbum
 from django.contrib.auth.models import User
-
+import json
+from datetime import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
 # Create your models here.
 class UserLogEntry(models.Model):
     #user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.SET_NULL,null=True, blank=True)
@@ -18,11 +25,15 @@ class UserLogEntry(models.Model):
     action = models.CharField(max_length=255)
     timestamp = models.DateTimeField(auto_now_add=True)
     extra = models.JSONField(default=dict)
-    def __str__(self):
-        newExtra = self.extra
-        del newExtra['a']
-        del newExtra['timestemp']
-        return self.timestamp.strftime("%d/%m/%Y, %H:%M:%S.%f")[:-3] + ' || ' + self.action + ' || ' + str(self.extra)
+    def __str__(self) -> str:
+        return self.action
+    
+    def admin_description(self):
+        return str(self.id) + ')\t \t' + self.timestamp.strftime("%H:%M:%S.%f")[:-3] + "\t - \t" + self.action
+        #newExtra = self.extra
+        #del newExtra['a']
+        #del newExtra['timestemp']
+        #return self.timestamp.strftime("%d/%m/%Y, %H:%M:%S.%f")[:-3] + ' || ' + self.action + ' || ' + str(self.extra)
     
 
     
@@ -45,6 +56,184 @@ class UserSessionLogger(models.Model):
         else:
             return '-'
     session_duration.short_description = _('session duration')
+    
+    def generate_telegram_message(self):
+        # UserSessionLogger.objects.first().generate_telegram_message()
+        data = self.analyze_user_session()
+        main_table = pt.PrettyTable([])
+        #user_table = pt.PrettyTable([])
+        #user_table.add_row(['שם משתמש', data['user']['username']])
+        #user_table.add_row(['שם העסק', data['user']['business_name']])
+        #user_table.add_row(['IP', data['user']['device']])
+        ret = '\r\n\r\n\r\n'
+        ret += '<b>תאריך:</b> ' + data['session']['t_start'].strftime("%d/%m/%Y, %H:%M:%S") + '\r\n'
+        ret += '<b>זמן באתר:</b> ' + data['session']['duration'] + '\r\n'
+        ret += '\r\n'
+        
+        ret += '<b>שם משתמש: </b> ' + data['user']['username'] + '\r\n'
+        ret += '<b>שם עסק: </b> ' + data['user']['business_name'] + '\r\n'
+        ret += '\r\n'
+        
+        ret += '<b> כתובת:</b> ' + data['user']['device'] + '\r\n'
+        ret += '<b>כמות פעולות:</b> ' + str(data['session']['logs_count']) + '\r\n'
+        cart = data['last_cart']
+        ret += '\r\n'
+        
+        
+        #cartTable = pt.PrettyTable(['עגלה שלא נשלחה'])
+        if(cart != None and len(cart) > 0):
+            ret += '<b>עגלה שלא נשלחה:</b>' + '\r\n'
+            ret += '<b>כמות פריטים:</b>' + str(cart.items.count()) + '\r\n'
+            for id in cart:
+                product = cart[id]
+                #cartTable.add_row([product['ti']])
+                ret += product['ti'] + '\r\n'
+        else:
+            ret += 'אין עגלה שלא נשלחה'
+
+        ret += '\r\n\r\n\r\n'
+        
+        sum_products_watch_time = {}
+        logs = data['session']['logs']
+        for idx, log  in enumerate(logs):
+            try:
+                if log['data']['t'] == 'open product':
+                    if(idx < logs.__len__() - 1):
+                        s_time = datetime.strptime(logs[idx+1]['data']['timestemp'],'%Y-%m-%dT%H:%M:%S.%fZ')
+                        e_time = datetime.strptime(log['data']['timestemp'],'%Y-%m-%dT%H:%M:%S.%fZ')
+                        duration = s_time-e_time#logs[idx+1]['data']['timestemp'] - 
+                        product_id = log['data']['w']['id']
+                        if product_id in sum_products_watch_time:
+                            sum_products_watch_time[product_id] += duration
+                        else:
+                            sum_products_watch_time[product_id] = duration
+            except:
+                pass
+        #ret += f'<code>{cartTable}</code>'
+        
+        sumTable = pt.PrettyTable(['שם המוצר','זמן','קטגוריה'])
+        sumTable.align = 'l'
+        sumTable.align['זמן'] = 'c'
+        chart_data = []
+        for id in sum_products_watch_time:
+            ducation = sum_products_watch_time[id]
+            product = CatalogImage.objects.get(id=id)
+            
+            sumTable.add_row([product.title[0:15], str(ducation)[:-3], product.albums.first().title])
+            chart_data.append({'name':product.title, 'value':ducation.total_seconds(), 'category':product.albums.first().title})
+        tableStr = str(sumTable)
+        tableStr = tableStr.replace('---+', '---+ת')
+        
+        ret += 'התעניינויות:' + "\r\n"
+        ret += f'<code>{tableStr}</code>'
+        chart = self.get_pie_cart(chart_data)
+        
+        
+        
+        return ret
+    def get_pie_cart(self,data):
+        df = pd.DataFrame(data)
+        from bidi.algorithm import get_display
+        import arabic_reshaper
+        print(df.head())
+        plt.figure(figsize=(19,14))
+        # nested pie chart of the product's categories, value and name:
+        
+        names = df.iloc[:,0]
+        categories = df.iloc[:,2]
+        heb_names = []
+        heb_categories = []
+        category_sums = []
+        colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'red', 'blue', 'green', 'orange', 'pink', 'purple', 'brown', 'grey', 'olive', 'teal', 'navy', 'maroon', 'lime', 'fuchsia', 'tan', 'aqua', 'silver', 'indigo', 'violet', 'black', 'white']
+        
+        for n in names:
+            reshaped_text = arabic_reshaper.reshape(n)
+            artext = get_display(reshaped_text)
+            heb_names.append(artext)
+        for c in categories:
+            reshaped_text = arabic_reshaper.reshape(c)
+            artext = get_display(reshaped_text)
+            heb_categories.append(artext)
+        df['heb_names'] = heb_names
+        df['heb_categories'] = heb_categories
+        plt.pie(df['value'], labels=df.iloc[:,3],colors=colors, autopct='%1.1f%%', shadow=False, startangle=90, radius=1)
+        #inner pie chart of the product's categories:
+        plt.pie(df['value'], labels=df.iloc[:,4], colors=colors, autopct='%1.1f%%', shadow=False, startangle=90, radius=0.75)
+        #plt.legend(loc='0', bbox_to_anchor=(1.5, 0.5), fontsize=20)
+        plt.show()
+        
+        plt.title('התעניינות במוצרים')
+        plt.tight_layout()
+        return plt
+    def send_telegram_message(self):
+        # UserSessionLogger.objects.get(id=85).send_telegram_message()
+        bot = telegram.Bot(token=TELEGRAM_BOT_TOKEN)
+        #for chat_id in self.chat_ids:
+        message = self.generate_telegram_message()
+        chat_id = '354783543'
+        print(bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML'))
+    
+    def analyze_user_session(self):
+        # UserSessionLogger.objects.first().analyze_user_session()
+        username = None
+        business_name = None
+        if self.user and not self.user.is_anonymous:
+            username = self.user.username
+            business_name = self.user.client.businessName
+        if username is None:
+            username = '-'
+            business_name = '-'
+        ret = {
+            'user': {
+                'username': username,
+                'business_name': business_name,
+                'uid': self.uid,
+                'device': self.device,
+            },
+            'session': {
+                'id': self.id,
+                't_start': self.session_start_timestemp,
+                't_end': self.session_end_timestemp,
+                'duration': self.session_duration(),
+                'logs_count': self.logs.count(),
+                'logs': [],
+            }
+        }
+        cart = {}
+        all_cart = {}
+        for log in self.logs.all():
+            ret['session']['logs'].append({
+                'id': log.id,
+                'data': log.extra,
+                'timestemp': log.timestamp,
+            })
+            try:
+                if(log.extra['t'] == 'add to cart'):
+                    cart[log.extra['w']['id']] = log.extra['w']
+                    all_cart[log.extra['w']['id']] = log.extra['w']
+                if(log.extra['t'] == 'remove from cart'):
+                    del cart[log.extra['w']['id']]
+                    
+                if(log.extra['t'] ==  'submit order'):
+                    cart = {}
+            except Exception as e:
+                print(e)
+                pass
+        
+        print(cart)
+        
+        ret['all_add_to_cart'] = all_cart
+        ret['last_cart'] = cart
+        return ret
+    
+    def admin_display_logs(self):
+        ret ='<div style="border:1px solid black"><ul>' +  '<li style="border-top:1px solid blue;">'.join([str(log.admin_description())+'</li>' for log in self.logs.all()])
+        ret += '</ul></div>'
+        return mark_safe(ret)
+        
+    
+    def get_logs(self):
+        return self.logs.all()
 
 
 
