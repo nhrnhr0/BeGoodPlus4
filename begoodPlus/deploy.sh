1df8df08620c7a482f2846bc1617d3b594d79567
Ubuntu <ubuntu@ip-172-31-23-234.us-east-2.compute.internal>
2021-11-30 05:01:08 +0000

    update deploy on production aws

--- begoodPlus/deploy.sh
+++ begoodPlus/deploy.sh
@@ -4,6 +4,7 @@ pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
 sudo supervisorctl restart gunicornNewMs2
-sudo supervisorctl restart begoodplus4-celery
+sudo supervisorctl restart begoodcelery
+sudo supervisorctl restart begoodcelerybeat
 sudo service nginx restart
 echo 'sudo service nginx restart was executed'
