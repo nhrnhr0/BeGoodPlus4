05f81056710c1f85b503046fd15f767775dbcc8f
nhrnhr0 <nhrnhr0@gmail.com>
2021-12-27 08:17:45 +0200

    production and dev settings, diffrent base server url (stil not working on watch)

--- svelte/src/consts/consts.js
+++ svelte/src/consts/consts.js
@@ -7,5 +7,5 @@ export const GET_CAMPAIN_PRODUCTS_URL = ADMIN_API_URL + 'get-campaign-products/'
 
 export const CLOUDINARY_BASE_URL = 'https://res.cloudinary.com/ms-global/image/upload/';
 
-export const BASE_URL = 'http://127.0.0.1:8000';//import.meta.env['VITE_SERVER_LOCATION'];
+export const BASE_URL = import.meta.env['VITE_SERVER_LOCATION'];
 export const SEARCH_API_URL = BASE_URL + '/search';
