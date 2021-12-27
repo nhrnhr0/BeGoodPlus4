1856be5f97b5af58a5d5260242c1c3a2dafe847c
nhrnhr0 <nhrnhr0@gmail.com>
2021-12-27 09:37:11 +0200

    t2

--- svelte/src/consts/consts.js
+++ svelte/src/consts/consts.js
@@ -8,5 +8,5 @@ export const GET_CAMPAIN_PRODUCTS_URL = ADMIN_API_URL + 'get-campaign-products/'
 export const CLOUDINARY_BASE_URL = 'https://res.cloudinary.com/ms-global/image/upload/';
 
 
-export const BASE_URL = 'https://catalog.boost-pop.com/';
+export const BASE_URL = 'https://catalog.boost-pop.com';
 export const SEARCH_API_URL = BASE_URL + '/search';
