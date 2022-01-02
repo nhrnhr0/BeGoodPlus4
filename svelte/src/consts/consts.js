377c89c0db3d7583a40c77f6e46106d7469f8563
nhrnhr0 <nhrnhr0@gmail.com>
2022-01-02 11:42:09 +0200

    change url to boost-pop

--- svelte/src/consts/consts.js
+++ svelte/src/consts/consts.js
@@ -8,6 +8,6 @@ export const GET_CAMPAIN_PRODUCTS_URL = ADMIN_API_URL + 'get-campaign-products/'
 export const CLOUDINARY_BASE_URL = 'https://res.cloudinary.com/ms-global/image/upload/';
 
 
-export const BASE_URL = 'http://127.0.0.1:8000'; // 'https://catalog.boost-pop.com';
+export const BASE_URL = 'https://catalog.boost-pop.com'; //'http://127.0.0.1:8000'; // 
 export const SEARCH_API_URL = BASE_URL + '/search';
 export const GET_PRODUCT_COST_PRICE_URL = BASE_URL + '/admin-api/get_product_cost_price/';
