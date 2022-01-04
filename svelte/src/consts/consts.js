f000f4050b820f45bef6fbc710d07069372f7648
nhrnhr0 <nhrnhr0@gmail.com>
2022-01-05 00:09:26 +0200

    looks ready for prod

--- svelte/src/consts/consts.js
+++ svelte/src/consts/consts.js
@@ -7,7 +7,11 @@ export const GET_CAMPAIN_PRODUCTS_URL = ADMIN_API_URL + 'get-campaign-products/'
 
 export const CLOUDINARY_BASE_URL = 'https://res.cloudinary.com/ms-global/image/upload/';
 
+var pathArray = import.meta.url.split( '/' );
+var protocol = pathArray[0];
+var host = pathArray[2];
 
-export const BASE_URL = 'https://catalog.boost-pop.com'; //'http://127.0.0.1:8000'; // 
+var url = protocol + '//' + host;
+export const BASE_URL =  url; //'https://catalog.boost-pop.com'; //'http://127.0.0.1:8000'; // 
 export const SEARCH_API_URL = BASE_URL + '/search';
 export const GET_PRODUCT_COST_PRICE_URL = BASE_URL + '/admin-api/get_product_cost_price/';
