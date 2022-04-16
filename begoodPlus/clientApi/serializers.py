245346db7c90ba4f8ba283cf9df6d03f865c20cc
nhrnhr0 <nhrnhr0@gmail.com>
2022-04-16 19:00:35 +0300

    fix user not auth client prices 500 error

--- begoodPlus/clientApi/serializers.py
+++ begoodPlus/clientApi/serializers.py
@@ -43,27 +43,31 @@ class VarientSerializer(serializers.ModelSerializer):
 
 class ImageClientApi(serializers.ModelSerializer):
     varients = VarientSerializer(read_only=True, many=True)
-    current_user = serializers.SerializerMethodField('_user')
+    #current_user = serializers.SerializerMethodField('_user')
     price = serializers.SerializerMethodField('_get_price')
     
     class Meta:
         model = CatalogImage
-        fields = ('id','title','description','cimage','colors','sizes','varients','can_tag','discount', 'albums','amountSinglePack','amountCarton', 'show_sizes_popup', 'client_price', 'out_of_stock', 'barcode', 'has_physical_barcode','current_user','price')
+        fields = ('id','title','description','cimage','colors','sizes','varients','can_tag','discount', 'albums','amountSinglePack','amountCarton', 'show_sizes_popup', 'client_price', 'out_of_stock', 'barcode', 'has_physical_barcode','price')
         filter_backends = [DjangoFilterBackend]
         filterset_fields = ['albums']
     # Use this method for the custom field
-    def _user(self, obj):
-        request = self.context.get('request', None)
-        if request:
-            return request.user.client.businessName if request.user.client else ''
+    # def _user(self, obj):
+    #     request = self.context.get('request', None)
+    #     if request:
+    #         if request.user.is_authenticated:
+    #             return request.user.client.businessName if request.user.client else ''
+    #         else:
+    #             return ''
     def _get_price(self, obj):
         request = self.context.get('request', None)
         if request:
-            if request.user.client:
-                tariff = request.user.client.tariff
-                price = obj.client_price + (obj.client_price * (tariff/100))
-                return round(price * 2) / 2 if price > 50 else "{:.2f}".format(price)
-            return obj.client_price
+            if request.user.is_authenticated:
+                if request.user.client:
+                    tariff = request.user.client.tariff
+                    price = obj.client_price + (obj.client_price * (tariff/100))
+                    return round(price * 2) / 2 if price > 50 else "{:.2f}".format(price)
+                return obj.client_price
         return 0
 class ColorClientApi(serializers.ModelSerializer):
     class Meta:
