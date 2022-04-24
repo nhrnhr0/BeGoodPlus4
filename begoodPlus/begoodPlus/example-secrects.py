eca91a660bb5dd706fd6c0a43bff14cc8e6a91d7
nhrnhr0 <nhrnhr0@gmail.com>
2022-04-24 14:17:08 +0300

    edit readme and adding example secrects.py

---
+++ begoodPlus/begoodPlus/example-secrects.py
@@ -0,0 +1,46 @@
+
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+MAIN_EMAIL_RECEIVER = 'nhrnhr0@gmail.com'
+
+SECRET_KEY = ''
+
+
+TWILIO_ACCOUNT_SID = ''
+TWILIO_AUTH_TOKEN = ''
+
+
+VAPID_PUBLIC_KEY = ''
+VAPID_PRIVATE_KEY = ''
+
+CLOUDINARY_NAME = ''
+CLOUDINARY_KEY = ''
+CLOUDINARY_SECRECT = ''
+DJANGO_SETTINGS_MODULE = 'begoodPlus.settings.development'# 'begoodPlus.settings.production'
+
+BROKER_USER = 'guest'
+BROKER_PASSWORD = 'guest'
+
+
+FLASHY_MY_API_KEY = ''
+FLASHY_MAIN_LIST_ID = ''
+WHATSAPP_NODE_SERVER_URL = 'http://localhost:3541'
+
+
+DB_ENGINE = 'django.db.backends.postgresql_psycopg2'
+DB_NAME = 'testdb5'
+DB_USER = 'postgres'
+DB_PASSWORD = 'password'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+
+SECRECT_BASE_MY_DOMAIN = '127.0.0.1:8000' # 'ms-global.co.il' # 'boost-pop.com'
+FULL_DOMAIN = 'http://127.0.0.1:8000' # 'https://catalog.boost-pop.com' # 'https://catalog.ms-global.co.il'
+MAPS_API_KEY = ''
+
+
+TELEGRAM_BOT_TOKEN = ''
+TELEGRAM_CHAT_ID_CARTS =''
+TELEGRAM_CHAT_ID_LEADS = ''
+TELEGRAM_CHAT_ID_QUESTIONS =''
+TELEGRAM_CHAT_ID_PRODUCT_PHOTO = ''
