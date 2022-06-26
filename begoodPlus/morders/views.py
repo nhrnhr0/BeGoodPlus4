5d869cbc60c8c5923a4cdd4ff104875fe6163996
nhrnhr0 <nhrnhr0@gmail.com>
2022-06-26 11:44:31 +0300

    fix docx export

--- begoodPlus/morders/views.py
+++ begoodPlus/morders/views.py
@@ -3,6 +3,7 @@ import datetime
 from gettext import Catalog
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
+from matplotlib.pyplot import annotate
 
 from catalogImages.models import CatalogImage
 from inventory.models import WarehouseStock
@@ -17,6 +18,7 @@ from django.shortcuts import render
 from io import BytesIO
 from django.http import HttpResponse
 from django.db import connection, reset_queries
+from django.db.models import F
 
 from django.template.loader import get_template
 #from docxtpl import DocxTemplate
@@ -75,7 +77,11 @@ def create_provider_docs(request):
         provider_requests_ids = json.loads(request.GET.get('ids'))
         objs = ProviderRequest.objects.filter(id__in=provider_requests_ids)
         print(objs)
-        vals = objs.values('provider__name','orderItem__product__title','size__size','varient__name','color__name',).annotate(total_quantity=Sum('quantity')).order_by('provider__name','orderItem__product__title','size__size','varient__name','color__name')
+        #orderItem__product__title =  orderItem.first()__product__title
+        vals = objs.values(ספק=F('provider__name'),מוצר=F('orderItem__product__title'),מידה=F('size__size'),מודל=F('varient__name'),צבע=F('color__name',))\
+                                .annotate(כמות=Sum('quantity'))\
+                                .order_by('ספק','מוצר','מידה','מודל','צבע')
+                
         
         
         
@@ -83,14 +89,14 @@ def create_provider_docs(request):
         print(vals)
         providers_split = {}
         for val in vals:
-            if val['provider__name'] not in providers_split:
-                providers_split[val['provider__name']] = {'products':[]}
-            providers_split[val['provider__name']]['products'].append(val)
+            if val['ספק'] not in providers_split:
+                providers_split[val['ספק']] = {'products':[]}
+            providers_split[val['ספק']]['products'].append(val)
         print(providers_split)
         for provider_name, data in providers_split.items():
             sizes_set = set()
             for product in data['products']:
-                sizes_set.add(product['size__size'])
+                sizes_set.add(product['מידה'])
             sizes_list = list(sizes_set)
             sizes_list.sort(key=lambda x: sizes[x])
             data['sizes_list'] = sizes_list
@@ -166,9 +172,9 @@ def create_providers_docx(doc_data):
         
         
         data = doc_data['products'] # [{'provider__name': 'defult provider', 'orderItem__product__title': 'חולצת דרייפיט שרוול ארוך', 'size__size': 'ONE SIZE', 'varient__name': None, 'color__name': 'שחור', 'total_quantity': 4}, {'provider__name': 'אוניברסל פרטס', 'orderItem__product__title': "בידורית 12 אינץ' power", 'size__size': '38', 'varient__name': None, 'color__name': 'no color', 'total_quantity': 10},...
-        indexs = ['orderItem__product__title','varient__name', 'color__name']
-        column = 'size__size'
-        value = 'total_quantity'
+        indexs = ['מוצר','מודל', 'צבע']
+        column = 'מידה'
+        value = 'כמות'
         pivot_table = PivotReshape(data=data, index=indexs, columns=[column], value=value)
         data = pivot_table.pivot()
         key_cols = pivot_table.col_keys_list
@@ -195,12 +201,12 @@ def create_providers_docx(doc_data):
         
         
         t = document.add_table( 1,len(headers_keys))
-        headers_keys = reversed(headers_keys)
+        header_len = len(headers_keys)
+        rev_headers_keys = reversed(headers_keys)
         t.direction = WD_TABLE_DIRECTION.RTL
         header = t.rows[0].cells
-        header_len = len(headers_keys)
-        for idx, head in enumerate(headers_keys):
-            header[header_len - idx] = str(head)
+        for idx, head in enumerate(rev_headers_keys):
+            header[header_len - idx - 1].text = str(head)
         
         # header[0].text = 'שם מוצר'
         # header[1].text = 'מודל'
@@ -211,8 +217,8 @@ def create_providers_docx(doc_data):
         for _, row_data in data.items():
             print(row_data)
             row = t.add_row()
-
-            for header_key in headers_keys:
+            rev_headers_keys = reversed(headers_keys)
+            for idx, header_key in enumerate(rev_headers_keys):
                 print(row_data['data'].get(header_key, None), ' - ', header_key)
                 row.cells[headers_keys.index(header_key)].text = str(row_data['data'].get(header_key, None))
         
