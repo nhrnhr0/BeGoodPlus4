8fc993410358bc13e7606e45ad535f25b1af0cff
nhrnhr0 <nhrnhr0@gmail.com>
2022-08-05 09:16:30 +0300

    working on setup

---
+++ setup/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# install supervisor
+sudo apt update && sudo apt install supervisor
+sudo apt install nginx
+sudo apt install python3.8-venv -y
+cd /home/ubuntu/
+git clone https://github.com/nhrnhr0/BeGoodPlus4.git
+cd BeGoodPlus4/
+python3 -m venv env
+cp -r ./setup/supervisor/* /etc/supervisor/conf.d/
+cp -r ./setup/nginx/* /etc/nginx/conf.d/
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl restart all
+sudo supervisorctl status
+sudo service nginx restart
+
