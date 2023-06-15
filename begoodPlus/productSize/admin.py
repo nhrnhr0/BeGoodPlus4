81a36bb6777a48cb46a203b3f86646cb964036cf
nhrnhr0 <nhrnhr0@gmail.com>
2023-06-15 18:46:06 +0300

    adding groups to ProductSizes

--- begoodPlus/productSize/admin.py
+++ begoodPlus/productSize/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from .models import ProductSize
 # Register your models here.
 
+
 class ProductSizeAdmin(admin.ModelAdmin):
-    list_display = ('size', 'code',)
+    list_display = ('id', 'size', 'code', 'order')
+    list_filter = ('code', )
+    search_fields = ('size', 'code', )
+    list_editable = ('size', 'code', 'order')
+
 
 admin.site.register(ProductSize, ProductSizeAdmin)
