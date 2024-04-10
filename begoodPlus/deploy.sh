5bb934ef75f8f4f5ab007f94bcd125ec8913ae27
nhrnhr0 <nhrnhr0@gmail.com>
2024-04-11 02:50:55 +0300

    update diploy

--- begoodPlus/deploy.sh
+++ begoodPlus/deploy.sh
@@ -3,9 +3,9 @@ source ../env/bin/activate
 pip install -r final_req.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-sudo supervisorctl restart begoodcelery
-sudo supervisorctl restart begoodcelerybeat
-sudo supervisorctl restart gunicornNewMs2
+sudo supervisorctl restart boxcelery
+sudo supervisorctl restart boxcelerybeat
+sudo supervisorctl restart gunicornBox
 sudo service nginx restart
 sudo supervisorctl status
 echo 'sudo service nginx restart was executed'
